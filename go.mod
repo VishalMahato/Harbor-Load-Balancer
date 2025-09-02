6f52b18b7f3088ed232fd30c98e6182fe03153c2
VishalMahato <mailmevishalmahato@gmail.com>
2025-09-02 07:33:01 +0530

    chore: scaffold repo + go.mod; add minimal main and rr atom

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/VishalMahato/Harbor-Load-Balancer
+
+go 1.25.0
---
+++ internal/lb/rr.go
@@ -0,0 +1,12 @@
+// roundrobin
+package lb
+
+import "sync/atomic"
+
+var rr uint64
+
+// Next returns an index in [0..n-1]
+func Next(n int) int {
+	i := atomic.AddUint64(&rr, 1)
+	return int(i % uint64(n))
+}
